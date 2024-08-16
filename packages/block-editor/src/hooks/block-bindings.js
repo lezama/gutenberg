772c2d03472f6c62dc24609e8216b69d7adaf677
Mario Santos <santosguillamot@gmail.com>
2024-08-16 14:46:59 +0200

    Make changes to the helper text

--- packages/block-editor/src/hooks/block-bindings.js
+++ packages/block-editor/src/hooks/block-bindings.js
@@ -334,9 +334,13 @@ export const BlockBindingsPanel = ( { name, metadata } ) => {
 						/>
 					) }
 				</ItemGroup>
-				<Text variant="muted">
-					{ __( 'Attributes connected to various sources.' ) }
-				</Text>
+				<ItemGroup>
+					<Text variant="muted">
+						{ __(
+							'Attributes connected to custom fields or other dynamic data.'
+						) }
+					</Text>
+				</ItemGroup>
 			</ToolsPanel>
 		</InspectorControls>
 	);
